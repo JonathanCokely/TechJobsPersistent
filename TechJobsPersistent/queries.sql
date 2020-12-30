86a3646704362ba9f833505caf0484477042993b
Cokely <JC032926@cerner.net>
2020-12-30 14:50:22 -0600

    Updated queries.sql file to include the queries for parts 1,2,3

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,12 @@
 --Part 1
+-- "List the columns and their data types in the Jobs table"
+-- MySQL Jobs table: column=Id type=int, column=Name type=longtext, column= EmployerId type=int
 
 --Part 2
+-- "Write a query to list the names of the employers in St. Louis City"
+-- SELECT NAME FROM EMPLOYERS WHERE LOCATION = "St. Louis City";
 
 --Part 3
-
+-- "Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order."
+-- "If a skill does not have a job listed, it should not be included in the results of this query."
+-- SELECT DISTINCT s.name, s.description FROM skills s JOIN jobskills js WHERE js.SkillId = s.Id ORDER BY s.Name;
