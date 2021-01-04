adc56e09655f464dccbad582f82db11f7739cec5
Cokely <JC032926@cerner.net>
2021-01-03 18:13:30 -0600

    Including AddSkillViewModel.cs

---
+++ TechJobsPersistent/ViewModels/AddSkillViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechJobsPersistent.ViewModels
+{
+    public class AddSkillViewModel
+    {
+        [Required(ErrorMessage = "Skill Name is required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Skill Description is required")]
+        [StringLength(50, MinimumLength = 3)]
+        public string Description { get; set; }
+    }
+}
